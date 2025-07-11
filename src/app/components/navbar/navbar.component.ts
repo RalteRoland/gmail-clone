2a471ab60aa14b979245cd2dce65c2b47934f5b5
Roland Lalhruaizela <rolandralte2000@gmail.com>
2025-07-11 08:15:26 +0100

    Status Dropdown update

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  currentStatus = 'Active';
 
+  statusColorMap: { [key: string]: string } = {
+    'Active': '#34a853',
+    'Do Not Disturb': '#ea4335',
+    'Away': '#fbbc04',
+    'Invisible': '#80868b'
+  };
+
+  setStatus(status: string) {
+    this.currentStatus = status;
+  }
 }
